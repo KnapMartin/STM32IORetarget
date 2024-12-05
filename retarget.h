0b1887dade52e19da77effaed248ad664e98d394
KnapMartin <martin_knap@yahoo.com>
2024-12-05 12:40:32 +0100

    implemented

---
+++ retarget.h
@@ -0,0 +1,55 @@
+/*
+ * retarget.h
+ *
+ *  Created on: Dec 5, 2024
+ *      Author: Martin Knap
+ */
+
+#ifndef INC_RETARGET_H_
+#define INC_RETARGET_H_
+
+
+#include <stdint.h>
+
+struct UART_HandleTypeDef;
+
+#define RTRGT_MAX_DELAY 100 // ms
+
+static UART_HandleTypeDef *s_huart = NULL;
+static HAL_StatusTypeDef (*s_tx_func)(UART_HandleTypeDef *huart,
+        const uint8_t *pData, uint16_t Size, uint32_t Timeout);
+static HAL_StatusTypeDef (*s_rx_func)(UART_HandleTypeDef *huart, uint8_t *pData,
+        uint16_t Size, uint32_t Timeout);
+
+void RTRGT_init(UART_HandleTypeDef *huart)
+{
+	s_huart = huart;
+}
+
+void RTRGT_set_tx_function(
+        HAL_StatusTypeDef (*tx_func_ptr)(UART_HandleTypeDef *huart,
+                const uint8_t *pData, uint16_t Size, uint32_t Timeout))
+{
+	s_tx_func = tx_func_ptr;
+}
+
+void RTRGT_set_rx_function(
+        HAL_StatusTypeDef (*rx_func_ptr)(UART_HandleTypeDef *huart,
+                uint8_t *pData, uint16_t Size, uint32_t Timeout))
+{
+	s_rx_func = rx_func_ptr;
+}
+
+int _write(int file, char *data, int len)
+{
+	s_tx_func(s_huart, (uint8_t*) data, len, RTRGT_MAX_DELAY);
+	return len;
+}
+
+int _read(int file, char *data, int len)
+{
+	s_rx_func(s_huart, (uint8_t*) data, len, RTRGT_MAX_DELAY);
+	return len;
+}
+
+#endif /* INC_RETARGET_H_ */
